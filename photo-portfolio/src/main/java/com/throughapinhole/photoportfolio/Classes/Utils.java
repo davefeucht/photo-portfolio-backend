791d3b0ef7550bc00ce3fefbdf362a8f72f1f54e
Dave Feucht <poetas@gmail.com>
2023-07-27 11:32:24 +0200

    Creates Category class and Utils class

---
+++ photo-portfolio/src/main/java/com/throughapinhole/photoportfolio/Classes/Utils.java
@@ -0,0 +1,11 @@
+package com.throughapinhole.photoportfolio.Classes;
+
+/**
+ * Contains constant values and utility methods for use in the project
+ */
+final class Utils {
+    /** Value for a Category which has no parent */
+    final static int CATEGORY_NO_PARENT = -1; 
+
+    private Utils() {}
+}
