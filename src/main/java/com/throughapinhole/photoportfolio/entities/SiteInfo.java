5b58b9d26af22cfad39a77f42101bca768b44420
Dave Feucht <poetas@gmail.com>
2023-08-01 10:41:27 +0200

    Changes media references to 'long' and removes unnecessary properties

--- src/main/java/com/throughapinhole/photoportfolio/entities/SiteInfo.java
+++ src/main/java/com/throughapinhole/photoportfolio/entities/SiteInfo.java
@@ -5,11 +5,8 @@ import lombok.Data;
 @Data
 public class SiteInfo {
     private String description;
-    private String gmt_offset;
-    private String home;
-    private int site_icon;
-    private String site_icon_url;
-    private int site_logo;
-    private String timezone_string;
+    private String gmtOffset;
+    private long siteIcon;
+    private long siteLogo;
     private String url;
 }
